e3fef53d5491ad60ef727d674bd12765f5114af7
mateusspp <teu.patricio@hotmail.com>
2022-04-02 17:42:57 -0300

    Mudanca no atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,5 +4,8 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send ('Você está na rota de atendimentos.')); // comando get, é um metodo que pega a requisição recebida nos antedimentos (dentro da porta 3000) e devolve uma resposta, req e res
     // Uso o post p/ enviar dados ao nosso servidor, digamos que o cliente precisa preencher um formulário, consequetemente ele precisa enviar dados x, então...
-    app.post('/atendimentos', (req, res) => res.send('Você está na rota de antedimentso e esta realizando um POST'));
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send  ('Você está na rota de antedimentso e esta realizando um POST.')
+    });
 }
