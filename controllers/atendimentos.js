32ee32debe5885de8ca0078d4df09c7a6e26dd54
mateusspp <teu.patricio@hotmail.com>
2022-04-09 16:20:21 -0300

    Adicionando Delete

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -22,4 +22,9 @@ module.exports = app => {
 
         Atendimento.altera(id,valores,res)
     })
+
+    app.delete('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.deleta(id, res)
+    })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -80,12 +80,23 @@ class Atendimento {
             if(erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(200).json(resultados)
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta (id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id = ?'
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
             }
         })
     }
 }
-
 
 
 module.exports = new Atendimento
